9b29bbd5aeb42e05f2df375a739b06b3d7a3f906
Léo <leo.hetsch@gmail.com>
2020-05-08 10:55:20 +0100

    FileReader implementation

--- lib/log_parser/file_reader.rb
+++ lib/log_parser/file_reader.rb
@@ -1,5 +1,22 @@
 # frozen_string_literal: true
 module LogParser
   class FileReader
+    class FileNotFound < RuntimeError
+    end
+
+    class InvalidFilename < RuntimeError
+    end
+
+    def read(filepath)
+      raise InvalidFilename if filepath.nil?
+
+      File.open(filepath, 'r') do |file|
+        file.each do |line|
+          yield line.strip
+        end
+      end
+    rescue Errno::ENOENT
+      raise FileNotFound
+    end
   end
 end
