2854edeceefa467d1394b125976e528a8e94a5cd
Léo <leo.hetsch@gmail.com>
2020-05-08 11:11:38 +0100

    LineParser implementation

--- lib/log_parser/line_parser.rb
+++ lib/log_parser/line_parser.rb
@@ -1,5 +1,23 @@
 # frozen_string_literal: true
 module LogParser
   class LineParser
+    require 'ipaddr'
+
+    def parse(line)
+      parts = line.split(' ')
+      return nil if parts.length != 2
+      return nil unless valid_ip?(parts.last)
+
+      parts
+    end
+
+    private
+
+    def valid_ip?(value)
+      IPAddr.new(value)
+      true
+    rescue IPAddr::InvalidAddressError
+      false
+    end
   end
 end
